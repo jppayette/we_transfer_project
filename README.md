d7ca0c3ea331a14404c71f142d34ec5664b25bf0
villefranche2019 <48442795+villefranche2019@users.noreply.github.com>
2019-03-29 09:46:18 +0100

    Update

--- README.md
+++ README.md
@@ -5,3 +5,18 @@ Il s'agit de la structure de base pour votre projet "We Transfert"
 # Projet
 
 Création d'un "clone" de wetransfert  ou de Dropbox
+
+# Éléments attendus
+
+* Formulaire pour le dépot du fichier
+* Function de génération d'un lien qui sera partageable sur les réseaux sociaux
+    * options Partager directement sur Facebook
+* Envoi d'un mail avec les champs ('Expediteur','Destinataire','Message','Fichiers')
+
+
+# Suggérés
+* Suppression du fichier et de l'entrée en base de données après 7 jours
+
+# Optionnels
+* Permettre à un utilisateur de se connecter et de voir ses "partages" en cours
+* Utilisation d'Ajax pour l'upload des fichiers
---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,87 @@
+<?php
+/*****************************************************
+* 
+* Author :
+* Version :
+* 
+* Description : Gestion des connexions utilisateurs
+*  
+* 
+*/
+
+/**
+ * Display a listing of the resource.
+ *
+ * @return \Illuminate\Http\Response
+ */
+function index()
+{
+    //
+}
+
+/**
+ * Show the form for creating a new resource.
+ *
+ * @return \Illuminate\Http\Response
+ */
+function create()
+{
+    //
+}
+
+/**
+ * Store a newly created resource in storage.
+ *
+ * @param  \Illuminate\Http\Request  $request
+ * @return \Illuminate\Http\Response
+ */
+function store(Request $request)
+{
+    //
+}
+
+/**
+ * Display the specified resource.
+ *
+ * @param  int  $id
+ * @return \Illuminate\Http\Response
+ */
+function show($id)
+{
+    //
+}
+
+/**
+ * Show the form for editing the specified resource.
+ *
+ * @param  int  $id
+ * @return \Illuminate\Http\Response
+ */
+function edit($id)
+{
+    //
+}
+
+/**
+ * Update the specified resource in storage.
+ *
+ * @param  \Illuminate\Http\Request  $request
+ * @param  int  $id
+ * @return \Illuminate\Http\Response
+ */
+function update(Request $request, $id)
+{
+    //
+}
+
+/**
+ * Remove the specified resource from storage.
+ *
+ * @param  int  $id
+ * @return \Illuminate\Http\Response
+ */
+function destroy($id)
+{
+    //
+}
+
